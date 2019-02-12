b3c7f08084f518f15128e01cf2f9b80be70efd46
Tycho Yacub <tychoyacub@gmail.com>
2019-02-12 12:00:34 -0800

    Basic Evaluation Func added

--- MiniMaxAI.java
+++ MiniMaxAI.java
@@ -1,9 +1,12 @@
 
 
-import AIModule;
 
 public class MiniMaxAI extends AIModule {
 
+	private static final int WIDTH = 7;
+	private static final int HEIGHT = 6;
+	private static final int MAX_INT = Integer.MAX_VALUE;
+	private static final int MIN_INT = Integer.MIN_VALUE;
 	@Override
 	public void getNextMove(GameStateModule game) {
 		chosenMove = 0;
@@ -13,7 +16,7 @@ public class MiniMaxAI extends AIModule {
 	}
 	
 	
-	startMinimax(GameStateModule game){
+	private void startMinimax(GameStateModule game){
 		int depth = 5;
 		chosenMove = minimax(game, depth);
 	}
@@ -34,7 +37,7 @@ public class MiniMaxAI extends AIModule {
 			return evaluateState(game);
 		}
 		//TODO Loop for Mins
-		for()
+		//for()
 		return currMax;
 	}
 	
@@ -44,13 +47,45 @@ public class MiniMaxAI extends AIModule {
 			return evaluateState(game);
 		}
 		//TODO Loop for Maxs
-		for()
+		//for()
 		return currMin;
 	}
 	
 	private int evaluateState(GameStateModule game) {
 		//TODO create eval func.
+		if(game.isGameOver()) {
+			if(game.getWinner() == 1) return MAX_INT;
+			else return MIN_INT;
+		}
+		int c = 0;
+		for(int i = 0; i <= WIDTH; i++) {
+			for(int j = 0; j <= HEIGHT; j++) {
+				//TODO add in boost for middle cols
+				c+=valueAdjacent(game, i, j);
+			}
+		}
 		return 0;
+	}
+	
+	private int valueAdjacent(GameStateModule game, int i, int j) {
+		int player = game.getAt(i, j);
+		int c = 0;
+		for(int dx = -1; dx <= 1; dx++) {
+			for(int dy = -1; dy <= 1; dy++) {
+				if(validTile(i + dx, j + dy)) {
+					if(game.getAt(i + dx, j + dy) == player) c++; 
+				}
+			}
+		}
+		if(player == 1) return c;
+		return -c;
+	}
+	
+	private boolean validTile(int x, int y) {
+		if(x < 0 || x >= WIDTH || y < 0 || y >= HEIGHT) {
+			return false;
+		}
+		else return true;
 	}
 
 }
